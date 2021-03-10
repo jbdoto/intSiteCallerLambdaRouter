51bd02a92391b77c32bc9e7942fae3c9977476c3
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-10 09:49:17 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+TAG=1.0.0
+IMAGE_NAME=intsitecaller-upload-handler
+ACCOUNT_URI=483158796244.dkr.ecr.us-east-1.amazonaws.com
+REPO_URI=$ACCOUNT_URI/$IMAGE_NAME
+IMAGE_URI=$REPO_URI:$TAG
+
+docker build . -t $IMAGE_NAME
+docker tag $IMAGE_NAME $IMAGE_URI
+aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 | docker login --username AWS --password-stdin $ACCOUNT_URI
+docker push $IMAGE_URI
+aws lambda  update-function-code --function-name $IMAGE_NAME --image-uri $IMAGE_URI --publish --profile=jdoto-ab3 --region=us-east-1
